c3dbd08f851c524c87852deb0f6d685ff0b26326
sujan manandhar <sujanmanandhar@sujans-Air.lan>
2020-02-04 18:58:54 -0600

    observables fixed

--- src/app/compos/product/product.service.ts
+++ src/app/compos/product/product.service.ts
@@ -1,38 +1,48 @@
 import {Injectable} from '@angular/core';
 import {AngularFirestore, AngularFirestoreCollection} from '@angular/fire/firestore';
 import {ProductsModel} from './products.model';
-import {EMPTY, Observable} from 'rxjs';
+import {EMPTY} from 'rxjs';
 import {catchError, map} from 'rxjs/operators';
-import {WishCart} from '../../models/models';
+import {throwError } from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
-  products$: Observable<ProductsModel[]>;
+  // itemCollection: AngularFirestoreCollection<ProductsModel>;
+  // wishCollection: AngularFirestoreCollection<WishCart>;
   errMessage: string;
-  itemCollection: AngularFirestoreCollection<ProductsModel>;
-  wishCollection: AngularFirestoreCollection<WishCart>;
-  constructor(private afs: AngularFirestore) {
-    this.itemCollection = this.afs.collection('items');
-    this.wishCollection = this.afs.collection('wish');
+  itemCollection = this.afs.collection('items');
+  wishCollection = this.afs.collection('wish');
+  constructor(private afs: AngularFirestore) {}
+
+  products$ = this.itemCollection.snapshotChanges().pipe(
+    map(changes => {
+      return changes.map(a => {
+        const resp = a.payload.doc.data() as ProductsModel;
+        resp.id = a.payload.doc.id;
+        return resp;
+      });
+    }),
+    catchError(this.handleError)
+  );
+
+  handleError(err: any) {
+    let errorMessage: string;
+    if (err.error instanceof ErrorEvent) {
+      // A client-side or network error occurred. Handle it accordingly.
+      errorMessage = `An error occurred: ${err.error.message}`;
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong,
+      errorMessage = `Backend returned code ${err.status}: ${err.body.error}`;
+    }
+    console.error(err);
+    return throwError(errorMessage);
   }
 
-  getProducts() {
-    this.products$ = this.itemCollection.snapshotChanges().pipe(
-      map(changes => {
-        return changes.map(a => {
-          const resp = a.payload.doc.data() as ProductsModel;
-          resp.id = a.payload.doc.id;
-          return resp;
-        });
-      }),
-      catchError(err => {
-        this.errMessage = err;
-        return EMPTY;
-      })
-    );
-  }
+
   addItem(item: ProductsModel) {
     this.itemCollection.add(item);
   }
